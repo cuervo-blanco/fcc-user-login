5a0cf49954d01b4f864cde4b762b6f357b6b9b13
cuervo-blanco <jaios.228@gmail.com>
2024-01-09 12:00:38 -0500

    Add: pug login message

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
 app.route('/').get((req, res) => {
-	res.render('index');
+	res.render('index', {title: "Hello", message: "Please log in"});
 });
 
 const PORT = process.env.PORT || 3000;
